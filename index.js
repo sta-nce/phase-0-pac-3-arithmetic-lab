63643c22c0aa04e3529a549698253fa4fb23d265
Stanslous <stancemuindi21@gmail.com>
2022-08-22 15:49:04 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,53 @@
+function add(a, b){
+    return(a+b);
+}
+
+console.log(add(100, 10));
+
+function subtract(a, b){
+    return(a-b);
+}
+
+console.log(subtract(100, 10));
+
+function multiply(a, b){
+    return(a*b);
+}
+
+console.log(multiply(100, 10));
+
+function divide(a, b){
+    return(a/b);
+}
+
+console.log(divide(100, 10));
+
+function increment(n){
+    return(n +=1);
+}
+
+console.log(increment(5));
+
+function decrement(n){
+    return(n -=1);
+} 
+
+console.log(decrement(5));
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+makeInt("10");
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+makeInt("hahaha");
+
+function preserveDecimal(n){
+    return parseFloat(n, 10);
+}
+
+preserveDecimal("4.4444");
